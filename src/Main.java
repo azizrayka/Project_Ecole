381238f5b6cb1101448967c33d5f0978a0ef15dd
Hsm <houssem.thabet@isimg.tn>
2026-05-12 14:25:02 +0100

    final

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import UI.HomeUI;
 
 public class Main {
-    // The main method must be public, static, and take an array of Strings as an argument
     public static void main(String[] args) {
-        // This creates the object and opens your UI
         new HomeUI();
     }
 }
